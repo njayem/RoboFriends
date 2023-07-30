245a0cf91a80c55c035bd55758d14080dc8547f8
njayem <nadine.el.mufti@gmail.com>
2023-07-30 16:40:44 -0400

    Fix ReactDOM root render

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,4 +4,6 @@ import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
