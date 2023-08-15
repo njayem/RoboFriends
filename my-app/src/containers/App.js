94217f3dd027ff9278993cc2994ad38b0ca791f4
njayem <nadine.el.mufti@gmail.com>
2023-08-15 03:13:10 -0400

    Replaced the App class component with React Hooks

--- my-app/src/containers/App.js
+++ my-app/src/containers/App.js
@@ -1,5 +1,5 @@
 // IMPORT ALL THE CHILDREN
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 // We export variables like this:
 import SearchBox from "../components/SearchBox";
@@ -30,16 +30,53 @@ import ErrorBoundry from "../components/ErrorBoundry";
 // STATES reside in the CONSTRUCTOR of the APP class component
 
 // App is a SMART component
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: "",
-		};
-		// Intialize your state
-		console.log("1", "constructor()");
-	}
+function App() {
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: "",
+	// 	};
+
+	//.........................................................//
+	// REACT HOOKS //
+	//.........................................................//
+	// We use array destructuring to get the values we want
+	// the useState() hook returns an array with 2 values
+	// The first value is the current state
+	// The second value is a function that allows us to change the state
+	// and we give it an initial state
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState("");
+	const [count, setCount] = useState(0);
+	//.........................................................//
+	// REACT HOOKS //
+	//.........................................................//
+
+	//.........................................................//
+	// LIFE CYCLE METHODS USING HOOKS //
+	//.........................................................//
+	// React runs the useEffect() function everytime it Renders
+	// To prevent useEffect() from running everytime we render
+	// we pass in a second argument to useEffect()
+	// This second argument is an array of values
+	// If any of the values in the array change, then the useEffect()
+	// function runs
+	// We can pass in an empty array to useEffect() to make it run
+	// only once
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => {
+				return response.json();
+			})
+			.then((users) => {
+				setRobots(users);
+			});
+		console.log(count);
+	}, [count]); // only run if count changes (we fetch each time we click!)
+	//.........................................................//
+	// LIFE CYCLE METHODS USING HOOKS //
+	//.........................................................//
 
 	// Whenever we have a component (class component specifically)
 	// that needs to leverage some API call to grab data from the internet
@@ -47,26 +84,26 @@ class App extends Component {
 	// WE USE THE (componentDidMount()) LIFECYCLE METHOD!!!
 	// Mounting means the first time a component is rendered on the page
 	// the moment its placed on the DOM
-	componentDidMount() {
-		// API calls that update your state
-		// THEN RE-RENDER UI!!!
-		console.log("3", "componentDidMount()");
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((response) => {
-				// return parsed JSON promise
-				return response.json();
-			})
-			.then((users) => {
-				this.setState(
-					() => {
-						return { robots: users };
-					},
-					() => {
-						console.log(this.state.robots);
-					}
-				);
-			});
-	}
+	// componentDidMount() {
+	// API calls that update your state
+	// THEN RE-RENDER UI!!!
+	// 	console.log("3", "componentDidMount()");
+	// 	fetch("https://jsonplaceholder.typicode.com/users")
+	// 		.then((response) => {
+	// 			 return parsed JSON promise
+	// 			return response.json();
+	// 		})
+	// 		.then((users) => {
+	// 			this.setState(
+	// 				() => {
+	// 					return { robots: users };
+	// 				},
+	// 				() => {
+	// 					console.log(this.state.robots);
+	// 				}
+	// 			);
+	// 		});
+	// }
 
 	// (SEARCHBOX) COMPONENT COMMUNICATES WITH (APP) COMPONENT //
 	//.........................................................//
@@ -74,15 +111,9 @@ class App extends Component {
 	// Use (this syntax) + (arrow functions) + (dismiss const)
 	// Changes the state of "searchfiled" to the value we write
 	//.........................................................//
-	onSearchChange = (event) => {
-		this.setState(
-			() => {
-				return { searchfield: event.target.value };
-			},
-			() => {
-				console.log(event.target.value);
-			}
-		);
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	};
 
 	// (APP) COMPONENT COMMUNICATES WITH (CARDLIST) COMPONENT //
@@ -91,28 +122,31 @@ class App extends Component {
 	// The result (state change; robots change) is passed as a prop
 	//.........................................................//
 
-	render() {
-		// Render your initial component UI
-		console.log("2", "render()");
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		});
+	// Render your initial component UI
+	// console.log("2", "render()");
 
-		return !robots.length ? (
-			<h1>Loading</h1>
-		) : (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<ErrorBoundry>
-						<CardList robots={filteredRobots} />
-					</ErrorBoundry>
-				</Scroll>
-			</div>
-		);
-	}
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
+
+	return !robots.length ? (
+		<h1>Loading</h1>
+	) : (
+		<div className="tc">
+			<h1 className="f1">RoboFriends</h1>
+			<button
+				onClick={() => {
+					setCount(count + 1);
+				}}>
+				Click Me!
+			</button>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<ErrorBoundry>
+					<CardList robots={filteredRobots} />
+				</ErrorBoundry>
+			</Scroll>
+		</div>
+	);
 }
-
 export default App;
