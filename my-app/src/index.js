c261e4b303830634208c2d252017cb7e3b31a382
njayem <nadine.el.mufti@gmail.com>
2023-08-17 22:26:12 -0400

    Update index.js to accomodate REACT 18

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
+
+// REACT 18 UPDATE: (previously) import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// OLD WAY:
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
